aa554f6f5f417c79a813c7d936afd7763297aadd
LeeYoungJun <dldudwns72@gmail.com>
2021-07-12 20:12:49 +0900

    repository 변경

---
+++ src/test/java/baseball/NumberTest.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class NumberTest {
+
+    @Test
+    @DisplayName("숫자 생성")
+    void create(){
+        BaseBallNumber baseBallNumber = new BaseBallNumber();
+
+    }
+
+}
