3acdb4bd19d0505959c02dd653139d22819853d0
LeeYoungJun <dldudwns72@gmail.com>
2021-07-26 19:44:14 +0900

    test (Ball class BALL)

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -19,8 +19,19 @@ public class BallTest {
     @DisplayName("공 위치와 숫자가 일치하면 STRIKE")
     void strike(){
         Ball ball = new Ball(1,9);
-        BallStatus status = ball.play(ball.getPosition(), ball.getBallNo());
+        BallStatus status = ball.play(1, 9);
         assertThat(status).isEqualTo(BallStatus.STRIKE);
     }
+
+    @Test
+    @DisplayName("공 위치가 다르며 숫자가 일치하면 BALL")
+    void ball(){
+        Ball ball = new Ball(1,9);
+        BallStatus status = ball.play(2,9);
+        assertThat(status).isEqualTo(BallStatus.BALL);
+    }
+
+
+
 
 }
