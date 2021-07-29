0f830a2e469c775ebb0ef6899ec0dbb852ac3ce0
LeeYoungJun <dldudwns72@gmail.com>
2021-07-30 00:09:05 +0900

    사용자에게 입력을 묻는 UI

--- src/main/java/baseball/ui/Game.java
+++ src/main/java/baseball/ui/Game.java
@@ -7,12 +7,21 @@ import java.util.*;
 
 public class Game {
     public static void main(String args[]) {
-        Random random = new Random();
 
-        System.out.println("세자리 숫자를 입력하세요");
+    }
 
-        int randomNumber = random.nextInt(9) +1 ;
+    public List<Integer> askNumbers(){
+        System.out.print("숫자를 입력해 주세요");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
 
+        List<Integer> numbers = new ArrayList<>();
+        input.split("");
 
+        for(String number : input.split("")){
+            numbers.add(Integer.valueOf(number));
+        }
+
+        return numbers;
     }
 }
