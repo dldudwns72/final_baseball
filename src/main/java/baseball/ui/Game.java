ddd1917ef75251de83ae12d664e5beaf638a6139
LeeYoungJun <dldudwns72@gmail.com>
2021-07-30 00:12:41 +0900

    사용자에게 입력을 받아 userBalls 생성

--- src/main/java/baseball/ui/Game.java
+++ src/main/java/baseball/ui/Game.java
@@ -10,13 +10,13 @@ public class Game {
 
     }
 
-    public List<Integer> askNumbers(){
+    // Main 메소드 안에 있음으로 해당 메소드를 사용하기 위해서는 정적 메소드로 선언해야한다.
+    public static List<Integer> askNumbers(){
         System.out.print("숫자를 입력해 주세요");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
 
         List<Integer> numbers = new ArrayList<>();
-        input.split("");
 
         for(String number : input.split("")){
             numbers.add(Integer.valueOf(number));
