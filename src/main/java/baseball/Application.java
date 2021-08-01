4dcf0d9d89fdd42a687c05cb470f0ab06986f8aa
LeeYoungJun <dldudwns72@gmail.com>
2021-08-01 15:56:24 +0900

    컴퓨터 볼과 사용자 볼을 비교하여 일치하는 만큼 count증가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,20 @@
 package baseball;
 
+import baseball.domain.Judgement;
 import baseball.domain.NumberGenerator;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args){
         NumberGenerator generator = new NumberGenerator();
         List<Integer> numbers = generator.createRandomNumber();
-        System.out.print(numbers);  // 이것은 왜 바로 [1,1,2] 형태로 바로 보이는가 ? integer이라?
+        System.out.println(numbers);  // 이것은 왜 바로 [1,1,2] 형태로 바로 보이는가 ? integer이라?
 
+        Judgement judgement = new Judgement();
+        int count = judgement.correctCount(Arrays.asList(1,5,3),Arrays.asList(1,2,3));
+
+        System.out.print("count" + count);
     }
 }
--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -4,10 +4,16 @@ import java.util.List;
 
 public class Judgement {
     public int correctCount(List<Integer> computer, List<Integer> player){
-        return 0;
+        int result = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            if(computer.contains(player.get(i))){
+                result++;
+            }
+        }
+        return result;
     }
 
-    public boolean hasPlcae(int place, int number){ // 숫자위치와 숫자 값을 받아 boolean 반환
+    public boolean hasPlcae(List<Integer> computer,int place, int number){
         return  false;
     }
 }
