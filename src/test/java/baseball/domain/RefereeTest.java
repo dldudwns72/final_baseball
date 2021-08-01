a8aa1f5752b751180eed43fb907d6c3eab2eb5c3
LeeYoungJun <dldudwns72@gmail.com>
2021-08-01 20:22:25 +0900

    Before Each를 통한 중복코드 제거

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +11,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RefereeTest {
 
+    private Referee referee;
+
+    @BeforeEach
+    void setUp(){
+        referee = new Referee();
+    }
+
     @Test
     @DisplayName("3스트라이크")
     void STRIKE(){
-        Referee referee = new Referee();
         String result = referee.compare(Arrays.asList(1,2,3),Arrays.asList(1,2,3));
         assertThat(result).isEqualTo("0 볼 3 스트라이크");
     }
@@ -21,7 +28,6 @@ public class RefereeTest {
     @Test
     @DisplayName("3볼")
     void BALL(){
-        Referee referee = new Referee();
         String result = referee.compare(Arrays.asList(3,1,2),Arrays.asList(1,2,3));
         assertThat(result).isEqualTo("3 볼 0 스트라이크");
     }
@@ -29,7 +35,6 @@ public class RefereeTest {
     @Test
     @DisplayName("아웃")
     void OUT(){
-        Referee referee = new Referee();
         String result = referee.compare(Arrays.asList(5,6,7),Arrays.asList(1,2,3));
         assertThat(result).isEqualTo("아웃");
     }
