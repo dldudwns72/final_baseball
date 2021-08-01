50b3ca40bb0db8fd65e3b5a47d3cd3a023b70ff1
LeeYoungJun <dldudwns72@gmail.com>
2021-08-01 16:10:45 +0900

    컴퓨터의 수와 플레이어의 수를 비교하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,23 +2,17 @@ package baseball;
 
 import baseball.domain.Judgement;
 import baseball.domain.NumberGenerator;
+import baseball.domain.Referee;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args){
-        NumberGenerator generator = new NumberGenerator();
-        List<Integer> numbers = generator.createRandomNumber();
-        System.out.println(numbers);  // 이것은 왜 바로 [1,1,2] 형태로 바로 보이는가 ? integer이라?
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(1,2,3),Arrays.asList(1,2,3));
 
-        Judgement judgement = new Judgement();
-        int count = judgement.correctCount(Arrays.asList(1,5,3),Arrays.asList(1,2,3));
-
-        System.out.println(count);
-
-        boolean place = judgement.hasPlcae(Arrays.asList(1,5,3),0,2);
-        System.out.println(place);
+        System.out.print(result);
 
     }
 }
