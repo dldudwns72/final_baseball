45e15e6c52056b91afcf85924cee77d5233a698c
LeeYoungJun <dldudwns72@gmail.com>
2021-09-02 00:56:34 +0900

    test : ball generate

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,2 +1,13 @@
-package baseball;public class Ball {
+package baseball;
+
+public class Ball {
+    private int ball;
+
+    public Ball(int ball){
+        this.ball = ball;
+    }
+
+    public int getBall() {
+        return ball;
+    }
 }
--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -1,2 +1,16 @@
-package baseball;public class BallTest {
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallTest {
+
+    @Test
+    @DisplayName("공 생성")
+    public void generateBall(){
+        Ball ball = new Ball(1);
+        assertThat(ball.getBall()).isEqualTo(1);
+    }
 }
