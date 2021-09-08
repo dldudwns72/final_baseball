2b35c4945297d37a47d33ed34199556edd73f334
LeeYoungJun <dldudwns72@gmail.com>
2021-09-08 23:46:59 +0900

    랜덤한 컴퓨터 볼 생성 및 사용자 입력 볼 생성

--- src/main/java/ui/InputView.java
+++ src/main/java/ui/InputView.java
@@ -1,15 +1,46 @@
 package ui;
 
+import baseball.Ball;
 import baseball.Balls;
+
+import java.util.*;
 
 public class InputView {
 
+    private static final String INPUT_USER_BALL = "숫자를 입력해주세요.";
+
     public static Balls createComputerBalls() {
-        return null;
+        List<Ball> balls = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = new Random().nextInt(10) - 1;
+            balls.add(new Ball(i, randomNumber));
+        }
+
+        Balls computerBalls = new Balls(balls);
+
+        return computerBalls;
     }
 
     public static Balls createUserBalls() {
-        return null;
+        System.out.println(INPUT_USER_BALL);
 
+        Scanner scanner = new Scanner(System.in);
+        List<String> userBallsSplit = Arrays.asList(scanner.nextLine().split(""));
+
+        return splitUserBalls(userBallsSplit);
+    }
+
+    private static Balls splitUserBalls(List<String> userBallsSplit){
+        List<Ball> balls = new ArrayList<>();
+
+        for (int i = 0; i < userBallsSplit.size(); i++) {
+            int ballNumber = Integer.parseInt(userBallsSplit.get(i));
+            balls.add(new Ball(i, ballNumber));
+        }
+
+        Balls userBalls = new Balls(balls);
+
+        return userBalls;
     }
 }
