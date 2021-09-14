f41975de60e86b54860e1a4685db854176e983d4
LeeYoungJun <dldudwns72@gmail.com>
2021-09-14 23:55:11 +0900

    resultView 수정

--- src/main/java/ui/ResultView.java
+++ src/main/java/ui/ResultView.java
@@ -9,7 +9,7 @@ public class ResultView {
     }
 
     public static void gameResult() {
-        System.out.println();
-    }
 
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
