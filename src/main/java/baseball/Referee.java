ce1d5bd50ea5406eac6024eb2ca446f43cccf380
LeeYoungJun <dldudwns72@gmail.com>
2021-09-18 23:51:41 +0900

    판별 Referee 테스트 완료

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -3,12 +3,14 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Referee {
 
     private Balls computerBalls;
     private Balls userBalls;
+
+    private List<String> strikeList = new ArrayList<>();
+    private List<String> ballList= new ArrayList<>();
 
     public Referee(Balls computerBalls, Balls userBalls) {
         this.computerBalls = computerBalls;
@@ -19,13 +21,41 @@ public class Referee {
         List<String> resultList = new ArrayList<>();
 
         for (int i = 0; i < computerBalls.getBalls().size(); i++) {
-            Ball computerBall = computerBalls.getBalls().get(i);
+            Ball userBall = userBalls.getBalls().get(i);
+            Judge judge = new Judge(computerBalls, userBall);
+            resultList.add(judge.discriminate());
+        }
+
+        return  getResult(resultList);
+    }
+
+    private String getResult(List<String> resultList) {
+
+        for (int i = 0; i < resultList.size(); i++) {
+
+            if (resultList.get(i).equals("STRIKE")) {
+                strikeList.add("STRIKE");
+            }
+
+            if (resultList.get(i).equals("BALL")) {
+                ballList.add("BALL");
+            }
 
         }
 
-        return null;
-    }
+        String strikeResult = "";
+        String ballResult = "";
 
+        if (strikeList.size() > 0) {
+            strikeResult = strikeList.size() + "STRIKE";
+        }
+
+        if (ballList.size() > 0) {
+            ballResult = ballList.size() + "BALL";
+        }
+
+        return strikeResult + ballResult;
+    }
 
 
 }
--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -21,15 +21,50 @@ public class RefereeTest {
     @Test
     @DisplayName("1볼")
     public void oneBall(){
-        userBalls = new Balls(Arrays.asList(new Ball(0,2),new Ball(1,6),new Ball(2,4)));
+        userBalls = new Balls(Arrays.asList(new Ball(0,2),new Ball(1,5),new Ball(2,6)));
 
         Referee referee = new Referee(computerBalls,userBalls);
 
         String gameResult = referee.gameResult();
 
         assertThat(gameResult).isEqualTo("1BALL");
-
     }
+
+    @Test
+    @DisplayName("2볼")
+    public void twoBall(){
+        userBalls = new Balls(Arrays.asList(new Ball(0,2),new Ball(1,3),new Ball(2,8)));
+
+        Referee referee = new Referee(computerBalls,userBalls);
+
+        String gameResult = referee.gameResult();
+
+        assertThat(gameResult).isEqualTo("2BALL");
+    }
+
+    @Test
+    @DisplayName("1스트라이크 1볼")
+    public void oneStrikeOneBall(){
+        userBalls = new Balls(Arrays.asList(new Ball(0,9),new Ball(1,1),new Ball(2,3)));
+
+        Referee referee = new Referee(computerBalls,userBalls);
+
+        String gameResult = referee.gameResult();
+
+        assertThat(gameResult).isEqualTo("1STRIKE1BALL");
+    }
+    @Test
+    @DisplayName("3스트라이크")
+    public void threeStrike(){
+        userBalls = new Balls(Arrays.asList(new Ball(0,1),new Ball(1,2),new Ball(2,3)));
+
+        Referee referee = new Referee(computerBalls,userBalls);
+
+        String gameResult = referee.gameResult();
+
+        assertThat(gameResult).isEqualTo("3STRIKE");
+    }
+
 
 
 
